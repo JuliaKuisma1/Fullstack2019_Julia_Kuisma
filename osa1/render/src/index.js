45d5328b3dd6cc3f1f5390606d35c8b31de98170
JuliaKuisma1 <julia.kuisma@student.lamk.com>
2019-06-24 12:43:23 +0300

    Some fixes

--- osa1/render/src/index.js
+++ osa1/render/src/index.js
@@ -1,37 +1,61 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-        {text}
-    </button>
-)
-
-const App = (props) => {
-    const [ counter, setCounter ] = useState(0)
-    const setToValue = (value) => setCounter(value)
-    
-    return (
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
         <div>
-        <Display counter={counter}/>
-        <Button
-          onClick={() => setToValue(counter + 1)}
-          text='plus'
-        />
-        <Button
-          onClick={() => setToValue(counter - 1)}
-          text='minus'
-        />
-        <Button
-          onClick={() => setToValue(0)}
-          text='zero'
-        />
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
       </div>
     )
-  }
+}
   
-    ReactDOM.render(<App />, 
-    document.getElementById('root'))
- 
+const Button = (props) => {
+    console.log(props)
+    const { handleClick, text } = props
+    return (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
+}
+  
+const App = (props) => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+        console.log('clicked left')
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+        console.log('clicked right')
+    }
+
+    return (
+        <div>
+        <div>
+            {left}
+            <Button handleClick={handleLeftClick} text='left' />
+            <Button handleClick={handleRightClick} text='right' />
+            {right}
+            <History allClicks={allClicks} />
+        </div>
+        </div>
+    )
+}
+  
+ReactDOM.render(<App />, 
+document.getElementById('root'))
