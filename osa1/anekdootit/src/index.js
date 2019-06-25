d0f5710f51e0179dbac9562456942fa46f15194f
JuliaKuisma1 <julia.kuisma@student.lamk.com>
2019-06-25 12:19:17 +0300

    anecdotes step 3 (stil no rerender)

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -6,28 +6,52 @@ const App = (props) => {
     const [random, setRandom] = useState(1)
     const [points, setPoints] = useState([0,0,0,0,0,0])
 
+    // randomize anecdotes shown
     const randomize = () => {
         var randomnum = Math.floor((Math.random() * 5) + 1)
-        // if randomizing same numbers, set a new one
+        // if randomizing same numbers, set a new one, still theres a chance to get same number
         if (random === randomnum) {
             setSelected(Math.floor((Math.random() * 5) + 1))
         }
+        // else set random num and selected
         else {
             setRandom(randomnum)
             setSelected(random)
         }
     }
 
+    // adding votes to hook
     const addVote = (selected) => {
         points[selected] += 1
         setPoints(points)
     }
+
+    // getting the value of most voted anecdotes
+    const mostViews = () => {
+        let max = points.reduce(function (prev, current) {
+            return prev > current ? prev : current
+        })
+        return max
+    } 
+
+    // getting the anecdote that was voted most
+    function arraySearch(points, max) {
+        for (var i=0; i < points.length; i++)
+            if (points[i] === max)                    
+                return i;
+        return false;
+    }
+
     return (
         <div>
-            {props.anecdotes[selected]} <br/>
+            <h1>Anecdote of the day</h1>
+            "{props.anecdotes[selected]}" <br/>
             <p>has { points[selected] } votes</p>
             <button onClick={() => addVote(selected)}>vote</button>
             <button onClick={() => randomize()}>next anecdote</button>
+            <h1>Anecdote with most votes</h1>
+            <p>{anecdotes[arraySearch(points, mostViews())]}</p>
+            <p>has {mostViews()} votes</p>
         </div>
     )
 }
