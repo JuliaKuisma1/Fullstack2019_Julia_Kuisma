d39b4c2ddfa81be039e6ba77323b17d5de708897
JuliaKuisma1 <julia.kuisma@student.lamk.com>
2019-06-27 09:39:42 +0300

    Still no check if name exists

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -1,39 +1,63 @@
 import React, { useState } from 'react';
 
 const App = (props) => {
-  const [ persons, setPersons ] = useState([ { name: 'Arto Hellas' }])
+  // array that contains all person information and new name and number
+  const [ persons, setPersons ] = useState([ { name: 'Arto Hellas', number: '040 123 4567' }])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const addName = (event) => {
+  // need to implement check that if name already exists
+  const addInformation = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName
+    const personObject = {
+      name: newName,
+      number: newNumber
     }
-    setPersons(persons.concat(nameObject))
+    setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
+  // handle name input changes
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value)
   }
 
+  // handle number input changes
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value)
+  }
+
+  // display persons array
   const rows = () => persons.map(person => 
-    <li key={person.name}>{person.name}</li>)
+    <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)
     
   return (
     <div>
       <h1>Phonebook</h1>
-      <form onSubmit={addName}>
-        <div>
-          Name: <input value={newName} onChange={handleNameChange}/>
-          <button type="submit">add</button> 
-        </div>
+      <form onSubmit={addInformation}>
+        <table>
+          <tbody>
+            <tr>
+              <td> Name: </td><td><input value={newName} onChange={handleNameChange}/></td>
+            </tr>
+            <tr>
+              <td>Number: </td><td><input value={newNumber} onChange={handleNumberChange}/></td>
+            </tr>
+            <tr>
+              <td><button type="submit">add</button></td> 
+            </tr>
+          </tbody>
+        </table>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {rows()}
-      </ul>
+      <table>
+        <tbody>
+          {rows()}
+        </tbody>
+      </table>
     </div> 
   )
 }
