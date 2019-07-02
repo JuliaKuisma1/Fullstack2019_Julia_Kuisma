11b9c9fc5ad0abda673c208afcaf7e131c4e3e03
JuliaKuisma1 <julia.kuisma@student.lamk.com>
2019-07-02 11:04:06 +0300

    maiden tiedot step2, sudan works

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// eslint-disable-next-line
 import css from './index.css';
+
+const Display = props => {
+  const { displayCountry } = props
+  // if country is undefined, it doesn't display anything
+  if (displayCountry === undefined) {
+    return null;
+  }
+  // else display country information
+  else {
+    return (
+      <div>
+        <h2 key={displayCountry.name}>{displayCountry.name}</h2>
+        <p key={displayCountry.capital}>Capital: {displayCountry.capital} <br/>
+        Population: {displayCountry.population}</p>
+        <h3>Languages</h3>
+        <ul>
+          {displayCountry.languages.map(language =>
+            <li key={language.name}>{language.name}</li>)}
+        </ul>
+        <img src={displayCountry.flag} alt={displayCountry.name} /> 
+      </div>
+    )
+  }
+}
 
 const App = (props) => {
   const [ countries, setCountries ] = useState([])
+  const [ displayCountry, setDisplayCountry ] = useState()
   const [ filter, setFilter ] = useState('')
   const [ filteredCountries, setFilteredCountries ] = useState([])
 
-  // get all information
+  // get all informationto countries array
   const hook = () => {
     console.log('effect')
     axios
@@ -25,32 +51,18 @@ const App = (props) => {
     if (filteredCountries.length > 10) {
       return <p>Too many matches, specify another filter</p>
     }
-    // if there is only one
-    else if (filteredCountries.length === 1) {
-      return filteredCountries.map(country => (
-        <div key={country.alpha2Code}>
-          <h2 key={country.name}>{country.name}</h2>
-          <p key={country.capital}>Capital: {country.capital} <br/>
-          Population: {country.population}</p>
-          <h3>Languages</h3>
-          <ul key={country.alpha2Code}>
-            {country.languages.map(language =>
-              <li key={language.name}>{language.name}</li>)}
-          </ul>
-          <img src={country.flag} alt={country.name} />
-        </div>
-      ))
-    }
+    // else show countries and show buttons 
     else {
       return filteredCountries.map(country => (
-        <p key={country.name}>{country.name}</p>
+        <div key={country.name}>
+          {country.name}<button onClick={() => showCountry(country)}>show</button><br/>
+        </div>
       ))
     }
   }
 
   const filterCountries = event => {
     event.preventDefault();
-    
     // check if filter is included in names, also change name to lowercase so it's more easier to compare
     if (filter !== '') {
       setFilteredCountries(countries.filter(country =>
@@ -61,18 +73,24 @@ const App = (props) => {
     }
   };
 
+  // set country as displayCountry
+  const showCountry = country => {
+    setDisplayCountry(country);
+  }
+
   // handle filter changes
   const handleFilterChange = event => setFilter(event.target.value);
 
   return (
     <div>
-      <h1>Maiden tiedot</h1>
+      <h1>Information about EU Countries</h1>
       <form onSubmit={filterCountries}>
         find countries: <input value={filter} onChange={handleFilterChange} />
       </form>
       <div>
         {checkFiltered()}
       </div>
+      <Display displayCountry={displayCountry} />
     </div> 
   )
 }
