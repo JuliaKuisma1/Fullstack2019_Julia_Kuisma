e6941d2db937ddd5943ba44a2d1e18653b9ce801
JuliaKuisma1 <julia.kuisma@student.lamk.com>
2019-07-05 13:22:35 +0300

    Modified singlepage app

--- osa2/singleapp/src/App.js
+++ osa2/singleapp/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
 import noteService from './servises/notes';
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happend...')
 
   const notesToShow = showAll
     ? notes
@@ -29,8 +32,11 @@ const App = (props) => {
           setNotes(notes.map(note => note.id !== id ? note : returnedNote));
         })
         .catch (error => {
-          alert(`the note '${note.content}' was already deleted from thr server`)
-          setNotes(notes.filter(n => n.id !== id))
+          setErrorMessage(`Note ${note.content} was already removed from the server`)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000);
+          setNotes(notes.filter(n => n.id !== id));
         })
   }
 
@@ -63,6 +69,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important': 'all'}
@@ -75,6 +82,7 @@ const App = (props) => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
